2f4978b5a28a45fdf0668d74ab44029e4fcb7658
Giovanni <giosvro@gmail.com>
2023-09-17 13:30:08 -0300

    🎉 Create Xcode Project

---
+++ HealthKit-Challenge/HealthKit-Challenge/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  HealthKit-Challenge
+//
+//  Created by Giovanni Severo on 17/09/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ HealthKit-Challenge/HealthKit-Challenge/HealthKit_ChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  HealthKit_ChallengeApp.swift
+//  HealthKit-Challenge
+//
+//  Created by Giovanni Severo on 17/09/23.
+//
+
+import SwiftUI
+
+@main
+struct HealthKit_ChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
